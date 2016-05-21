d2b3e9faa707f464dd78d1850b7dd096d71b35bc
Jesse Squires <jesse.d.squires@gmail.com>
2016-05-21 11:32:12 -0700

    initial empty project

---
+++ MessageKit/Sources/MessageData.swift
@@ -0,0 +1,19 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://messagekit.github.io
+//
+//
+//  GitHub
+//  https://github.com/MessageKit/MessageKit
+//
+//
+//  License
+//  Copyright (c) 2016-present Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import Foundation
